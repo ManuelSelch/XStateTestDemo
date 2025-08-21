89afee9f4f2de2638b05412b2769ca6f36fe0429
Manuel Selch <manuel@selch.de>
2025-08-21 20:15:45 +0200

    demo tests

--- test/machine.ts
+++ test/machine.ts
@@ -1,25 +1,22 @@
 import { createTestModel } from '@xstate/graph';
 import { createMachine } from 'xstate';
 
-var myValue = false;
-
 export const toggleMachine = createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QBcD2UoBswDpbIEMAnZAYgGNMBLcgawG0AGAXUVAAdVYrkrUA7NiAAeiALQAmAKw4AbFIAsigIyNlU2dIA0IAJ6IAHAGYcEhQE5zBi4yMSA7LPNSAvi51oM2HEQCu-fip+KApqOiZWJBBObl4BIVEECU0cZXtzWQN7KR19BGNTCysbO0dnN3cQflQIOCFPLDAhGJ4+QSjEyUZGOXNlCWs0jKycvXEZKW61SYz7CSNGBXs3D3RGvEISZq5W+I7xdQle-sH0zOzcwxMzS2tzWwcnV0qG7z8AoKht2LaExGVlLIcIx5LJ7EYDAN+qM8gUbsV7qUnisQK9cGB+BBvrt2qBEhCgeYIcYJCD1Ap+kZLvllDhLLcSo9yhUgA */
   id: 'toggle',
   initial: 'start',
   states: {
     start: {
       on: {
-        CLICK: 'end',
+        click: 'running',
       },
-      meta: {
-        test: () => !myValue 
+    },
+    running: {
+      on: {
+        click: 'end'
       }
     },
-    end: {
-      meta: {
-        test: () => myValue
-      }
-    },
+    end: {type: "final"},
   },
 });
 
--- test/test.ts
+++ test/test.ts
@@ -2,10 +2,21 @@ import { toggleModel } from "./machine";
 
 const plans = toggleModel.getSimplePaths();
 
+var myValue = 0;
+
 describe("xstate tests", () => {  
   plans.forEach((plan) => {
     it(plan.description, async () => {
-        await plan.test({});
+      await plan.test({
+        states: {
+          start: () => expect(myValue).toBe(0),
+          running: () => expect(myValue).toBe(1),
+          end: () => expect(myValue).toBe(2)
+        },
+        events: {
+          click: () => { myValue++ }
+        }
+      });
     });
   });
 })
